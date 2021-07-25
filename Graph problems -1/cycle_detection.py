13904f32405c1e23f2a33cfdbc01bc82882ebee4
Praveen.S <praveen2cheer@gmail.com>
2021-07-25 16:33:32 +0800

    detectycle1 issue fixed and detectycle2 code added

--- Graph problems -1/cycle_detection.py
+++ Graph problems -1/cycle_detection.py
@@ -1,5 +1,5 @@
 import grapg_utils as utils
-import queue as q
+from queue import Queue
 
 def auxDfs(u, parent, visit, inp):
     visit[u] = 1
@@ -7,7 +7,7 @@ def auxDfs(u, parent, visit, inp):
         if(inp[u][v]==1):
             if(visit[v]==0): #forward edge
                 visit[v] = 1
-                if(auxDfs(u, v, visit, inp)):
+                if(auxDfs(v, u, visit, inp)):
                     return True
             else: #back edge
                 if(v != parent):
@@ -23,7 +23,35 @@ def detectCycle1(inp):
             if(auxDfs(u, u, visit, inp)):
                 return True
     return False
-            
+#================================================================
+def auxBfs(u, visit, inp):
+    q = Queue()
+    q.put(u)
+    visit[u] = 1   #in the queue
+    
+    while(not q.empty()):
+        u = q.get()
+        visit[u] = 2 #taking out of the queue
+        for v in range(len(inp)):
+           if(inp[u][v] == 1):
+               if(visit[v] == 0):  #not Visited 
+                   q.put(v)
+                   visit[v] = 1
+               else:
+                   if(visit[v] == 1): #Visited 
+                      return True
+    return False
+    
+def detectCycle2(inp):
+    n = len(inp)
+    visit = [0] * n
+    for u in range(n):
+        if(visit[u] == 0):
+            if(auxBfs(u, visit, inp)):
+                return True
+    return False
+
+           
 def main():
    # n = int(sys.argv[1])
     n = 4
@@ -37,13 +65,13 @@ def main():
 # ]
 #=============================================================================
    # special case cycle exists
-    inp = [[0, 1, 0, 0], [1, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 0]]
-    #inp = [[0, 1],[1, 0]]
-    #inp = utils.randomUndirectedGraph(n)
+    #inp = [[0, 1, 0, 0], [1, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 0]]
+   # inp = [[0, 1],[1, 0]]
+    inp = utils.randomUndirectedGraph(n)
 
    # utils.display(inp)
     print(detectCycle1(inp))
-    #print(detectCycle2(inp))
+    print(detectCycle2(inp))
     #print(detectCycle3(inp))
     
 if __name__=="__main__":
